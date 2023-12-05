cbbb1072f740b6003a7f81d2cf8346fec6ecf310
lanorummar <marcelano2121@gmail.com>
2023-12-05 16:36:44 +0700

    first commit

---
+++ application/views/admin/home/index.php
@@ -0,0 +1,45 @@
+<div id="vue-home">
+	<!------- Information Widget - 2  Start ----------->
+	<div class="row info-widget-2 no-padding">
+		<div class="col-sm-4">
+			<div class="widget-2-cover color-1 row no-margin">
+				<div class="col-sm-9">
+					<h4>{{user}}</h4>
+					<span> Total Users </span>
+				</div>
+				<div class="col-sm-3 no-padding">
+					<div class="widget-icon">
+						<i class="fas fa-cubes"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="widget-2-cover color-2 row no-margin">
+				<div class="col-sm-9">
+					<h4>{{post}}</h4>
+					<span>Total Posts</span>
+				</div>
+				<div class="col-sm-3 no-padding">
+					<div class="widget-icon">
+						<i class="fas fa-luggage-cart"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="widget-2-cover color-3 row no-margin">
+				<div class="col-sm-9">
+					<h4>{{replay}}</h4>
+					<span>Today Reply</span>
+				</div>
+				<div class="col-sm-3 no-padding">
+					<div class="widget-icon">
+						<i class="far fa-money-bill-alt"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
